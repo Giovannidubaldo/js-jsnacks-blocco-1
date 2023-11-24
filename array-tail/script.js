ce01030a8b1e3f4b6546dd8804942855e938a5a5
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-24 17:29:22 +0100

    Added exercise array-tail

---
+++ array-tail/script.js
@@ -0,0 +1,20 @@
+// Punto 1
+let array = [];
+console.log(array);
+
+// Punto 2
+let utents = parseInt(prompt('Quanti elementi vuoi inserire?'));
+console.log('Hai inserito: ' + utents);
+
+// Punto 3
+for(let i=0; i<utents; i++){
+    let numbers = Math.round(Math.random()*100);
+    array.push(numbers);
+}
+
+console.log(array);
+
+// Punto 4
+const five_elements = array.slice(5,utents); /* da rivedere */
+console.log(five_elements);
+
